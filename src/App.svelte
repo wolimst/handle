b4add39023a3127938db4526661d356751b06e30
wolimst <64784258+wolimst@users.noreply.github.com>
2022-09-02 19:14:43 +0900

    Implement page routing and top navbar

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Nav from '@/Nav.svelte'
+  import { PRODUCTION, ROUTES } from '@/constants'
+  import FontViewer from '@/routes/FontViewer.svelte'
+  import Home from '@/routes/Home.svelte'
+  import { Router, Route } from 'svelte-routing'
+</script>
+
+<Router>
+  <Nav />
+
+  <main class="tw-container tw-mx-auto tw-pt-6">
+    <Route path={ROUTES.home}>
+      <Home />
+    </Route>
+    {#if !PRODUCTION}
+      <Route path={ROUTES.fontViewer}>
+        <FontViewer />
+      </Route>
+    {/if}
+  </main>
+</Router>
