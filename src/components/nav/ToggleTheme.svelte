3293736abfc10e5e5c40947ecdc65f5ef1561277
wolimst <64784258+wolimst@users.noreply.github.com>
2022-09-02 19:06:45 +0900

    Implement dark theme and update global css

---
+++ src/components/nav/ToggleTheme.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import ClickButton from '@/components/ui/core/ClickButton.svelte'
+  import ThemeDarkIcon from '@/components/ui/icons/ThemeDark.svelte'
+  import ThemeLightIcon from '@/components/ui/icons/ThemeLight.svelte'
+  import { config } from '@/stores/app'
+
+  function toggle() {
+    config.toggleTheme()
+  }
+</script>
+
+<ClickButton on:click={toggle}>
+  {#if $config.darkTheme}
+    <ThemeLightIcon />
+  {:else}
+    <ThemeDarkIcon />
+  {/if}
+</ClickButton>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@ module.exports = {
   content: ['./index.html', './src/**/*.{svelte,js,ts}'],
   darkMode: ['class', '[data-mode="dark-theme"]'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'app-bg': 'var(--background-color)',
+        'app-text': 'var(--text-color)',
+        'app-text-secondary': 'var(--text-color-secondary)',
+        'app-anchor-text': 'var(--anchor-text-color)',
+        'app-anchor-button': 'var(--text-color)',
+        'app-modal-bg': 'var(--modal-background-color)',
+        'app-primary': 'var(--primary-color)',
+        'app-secondary': 'var(--secondary-color)',
+        'app-neutral': 'var(--neutral-color)',
+        'app-wordle-correct': 'var(--wordle-correct-color)',
+        'app-wordle-present': 'var(--wordle-present-color)',
+        'app-wordle-absent': 'var(--wordle-absent-color)',
+      },
+    },
   },
   plugins: [],
   prefix: 'tw-',
