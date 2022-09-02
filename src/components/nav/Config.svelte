491eb3db15b293d6066e577ba7b3d0a4ef8d00c8
wolimst <64784258+wolimst@users.noreply.github.com>
2022-09-02 19:03:18 +0900

    Implement UI components

---
+++ src/components/nav/Config.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import ClickButton from '@/components/ui/core/ClickButton.svelte'
+  import Modal from '@/components/ui/core/Modal.svelte'
+  import Toggle from '@/components/ui/core/Toggle.svelte'
+  import ConfigIcon from '@/components/ui/icons/Config.svelte'
+
+  let open = false
+
+  function handleClick() {
+    open = !open
+  }
+</script>
+
+<ClickButton on:click={handleClick}>
+  <ConfigIcon />
+</ClickButton>
+
+<Modal bind:open title="TODO" widthRem={30} heightRem={30}>
+  <Toggle>Hello</Toggle>
+</Modal>
