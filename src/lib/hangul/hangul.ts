313b4180e3ac1852f040ae07e5ae9abd2e0c1f6f
wolimst <64784258+wolimst@users.noreply.github.com>
2022-09-02 13:13:54 +0900

    Implement hangul syllable decomposition library

---
+++ src/lib/hangul/hangul.ts
@@ -0,0 +1,164 @@
+import type { Consonant, Syllable, Vowel, Word } from './types'
+
+// Hangul syllables unicode block range: 0xac00..0xd7af
+export const FIRST_SYLLABLE = 0xac00
+export const LAST_SYLLABLE = 0xd7a3 // 0xd7a4..0xd7af are unused code points
+
+function isHangulSyllables(str: string): boolean {
+  return Array.from(str).every((codePointChar) => {
+    const codePoint = codePointChar.codePointAt(0)
+    return (
+      codePoint && FIRST_SYLLABLE <= codePoint && codePoint <= LAST_SYLLABLE
+    )
+  })
+}
+
+// Hangul Jamo unicode block range: 0x1100..0x11ff
+// Contains old consonants and vowels, such as 'ㅸ'
+const FIRST_JAMO = 0x1100
+const LAST_JAMO = 0x11ff
+
+function isHangulJamo(str: string): boolean {
+  return Array.from(str).every((codePointChar) => {
+    const codePoint = codePointChar.codePointAt(0)
+    return codePoint && FIRST_JAMO <= codePoint && codePoint <= LAST_JAMO
+  })
+}
+
+const leadingConsonants: readonly Consonant[] = [
+  'ㄱ',
+  'ㄲ',
+  'ㄴ',
+  'ㄷ',
+  'ㄸ',
+  'ㄹ',
+  'ㅁ',
+  'ㅂ',
+  'ㅃ',
+  'ㅅ',
+  'ㅆ',
+  'ㅇ',
+  'ㅈ',
+  'ㅉ',
+  'ㅊ',
+  'ㅋ',
+  'ㅌ',
+  'ㅍ',
+  'ㅎ',
+]
+
+const vowels: readonly Vowel[][] = [
+  ['ㅏ'],
+  ['ㅐ'],
+  ['ㅑ'],
+  ['ㅒ'],
+  ['ㅓ'],
+  ['ㅔ'],
+  ['ㅕ'],
+  ['ㅖ'],
+  ['ㅗ'],
+  ['ㅗ', 'ㅏ'],
+  ['ㅗ', 'ㅐ'],
+  ['ㅗ', 'ㅣ'],
+  ['ㅛ'],
+  ['ㅜ'],
+  ['ㅜ', 'ㅓ'],
+  ['ㅜ', 'ㅔ'],
+  ['ㅜ', 'ㅣ'],
+  ['ㅠ'],
+  ['ㅡ'],
+  ['ㅡ', 'ㅣ'],
+  ['ㅣ'],
+]
+
+const trailingConsonants: readonly Consonant[][] = [
+  [],
+  ['ㄱ'],
+  ['ㄲ'],
+  ['ㄱ', 'ㅅ'],
+  ['ㄴ'],
+  ['ㄴ', 'ㅈ'],
+  ['ㄴ', 'ㅎ'],
+  ['ㄷ'],
+  ['ㄹ'],
+  ['ㄹ', 'ㄱ'],
+  ['ㄹ', 'ㅁ'],
+  ['ㄹ', 'ㅂ'],
+  ['ㄹ', 'ㅅ'],
+  ['ㄹ', 'ㅌ'],
+  ['ㄹ', 'ㅍ'],
+  ['ㄹ', 'ㅎ'],
+  ['ㅁ'],
+  ['ㅂ'],
+  ['ㅂ', 'ㅅ'],
+  ['ㅅ'],
+  ['ㅆ'],
+  ['ㅇ'],
+  ['ㅈ'],
+  ['ㅊ'],
+  ['ㅋ'],
+  ['ㅌ'],
+  ['ㅍ'],
+  ['ㅎ'],
+]
+
+interface DecomposedSyllable {
+  leadingConsonant: Consonant
+  vowels: Vowel[]
+  trailingConsonants: Consonant[]
+}
+
+/**
+ * Decompose single Hangul syllable to a leading consonant, 1 or 2 vowels and
+ * 0-2 trailing consonants.
+ *
+ * Vowels and trailing consonants are sorted in the keyboard typing order.
+ *
+ * Input string containing non Hangul syllable might cause undefined behavior.
+ *
+ * @param str a string consists of single Hangul syllable
+ */
+function decomposeSyllable(str: string): DecomposedSyllable {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const syllableIndex = str.codePointAt(0)! - FIRST_SYLLABLE
+  const syllablesPerLeadingConsonant = vowels.length * trailingConsonants.length
+  const l = Math.floor(syllableIndex / syllablesPerLeadingConsonant)
+  const v = Math.floor(
+    (syllableIndex % syllablesPerLeadingConsonant) / trailingConsonants.length
+  )
+  const t =
+    (syllableIndex % syllablesPerLeadingConsonant) % trailingConsonants.length
+  return {
+    leadingConsonant: leadingConsonants[l],
+    vowels: [...vowels[v]],
+    trailingConsonants: [...trailingConsonants[t]],
+  }
+}
+
+/**
+ * Return code points length of the given string
+ */
+export function getCodePointLength(str: string): number {
+  return Array.from(str).length
+}
+
+/**
+ * Create {@link Word} object from string that contains Hangul syllables.
+ *
+ * Characters that are not Hangul syllables will be ignored.
+ *
+ * @param str a string containing Hangul syllables
+ */
+export function toWord(str: string): Word {
+  const syllableList: string[] = Array.from(str).filter(isHangulSyllables)
+  const syllableObjects: Syllable[] = syllableList.map((syllableStr) => ({
+    value: syllableStr,
+    ...decomposeSyllable(syllableStr),
+  }))
+
+  return {
+    value: syllableList.join(''),
+    length: syllableObjects.length,
+    syllables: syllableObjects,
+  }
+}
---
+++ src/lib/hangul/index.ts
@@ -0,0 +1,2 @@
+export * from './hangul'
+export * from './types'
