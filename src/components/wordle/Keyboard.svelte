62b1ae75070ce1f1b2ec1f01725f2383915b90eb
wolimst <64784258+wolimst@users.noreply.github.com>
2022-09-05 20:25:41 +0900

    Implement preliminary wordle UI components

---
+++ src/components/wordle/Keyboard.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import * as Wordle from '@/lib/wordle'
+
+  export let keyboard: Wordle.Keyboard
+</script>
+
+<form on:submit|preventDefault on:input>
+  <input type="text" bind:value={keyboard.value} />
+</form>
