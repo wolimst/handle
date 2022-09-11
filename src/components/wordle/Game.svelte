0e0c9a6c7ebccfcdf53cda57f30e0c2c177f87ad
wolimst <64784258+wolimst@users.noreply.github.com>
2022-09-11 20:23:17 +0900

    Display fixed number of wordles in a row

--- src/components/wordle/Game.svelte
+++ src/components/wordle/Game.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Guess from './Guess.svelte'
   import Keyboard from './Keyboard.svelte'
+  import { N_WORDLES_PER_ROW } from '@/constants'
   import * as Wordle from '@/lib/wordle'
 
   export let nWordles: number
@@ -9,6 +10,18 @@
 
   let game: Wordle.Game = new Wordle.Game(nWordles, nGuesses, answerLength)
   let keyboardValue = game.keyboard.value
+
+  const nWordlesPerRow = N_WORDLES_PER_ROW[answerLength]
+  const nRows = Math.ceil(game.nWordles / nWordlesPerRow)
+
+  function nWordlesThisRow(rowIndex: number): number {
+    const nWordlesRemaining = game.nWordles - rowIndex * nWordlesPerRow
+    if (nWordlesRemaining > nWordlesPerRow) {
+      return nWordlesPerRow
+    } else {
+      return nWordlesRemaining
+    }
+  }
 
   function onInput() {
     keyboardValue = game.keyboard.value
@@ -21,34 +34,32 @@
   }
 </script>
 
-<div
-  class="tw-container tw-inline-flex tw-flex-col tw-items-center tw-justify-between"
->
-  <div class="tw-flex tw-flex-row tw-flex-wrap tw-gap-8 tw-justify-center">
-    {#each game.data.wordleData as wordle}
-      <div class="tw-inline-flex tw-flex-col tw-gap-2 tw-items-center">
-        {#each wordle.guessResults as guessResult}
-          <Guess guess={guessResult} answerLength={game.answerLength} />
-        {/each}
-        {#if wordle.status === 'playing'}
-          {#if game.remainingGuesses > 0}
-            <Guess guess={keyboardValue} answerLength={game.answerLength} />
-          {/if}
-          {#if game.remainingGuesses > 1}
-            {#each { length: game.remainingGuesses - 1 } as _}
-              <Guess answerLength={game.answerLength} />
-            {/each}
-          {/if}
-        {/if}
-      </div>
-    {/each}
-  </div>
+<div class="tw-container tw-mb-12">
+  {#each { length: nRows } as _, rowIndex}
+    <div class="tw-mb-6">
+      {#each { length: game.nGuesses } as _, guessIndex}
+        <div
+          class="tw-flex tw-flex-nowrap tw-justify-center tw-gap-4 tw-mx-2 tw-my-1"
+        >
+          {#each { length: nWordlesThisRow(rowIndex) } as _, colIndex}
+            {@const wordleIndex = nWordlesPerRow * rowIndex + colIndex}
+            {@const wordle = game.data.wordleData[wordleIndex]}
 
-  <div class="tw-my-4">
-    <Keyboard
-      keyboard={game.keyboard}
-      on:submit={submitGuess}
-      on:input={onInput}
-    />
-  </div>
+            {#if guessIndex < wordle.guessResults.length}
+              <Guess
+                guess={wordle.guessResults[guessIndex]}
+                answerLength={game.answerLength}
+              />
+            {:else if wordle.status === 'playing' && guessIndex === wordle.guessResults.length}
+              <Guess guess={keyboardValue} answerLength={game.answerLength} />
+            {:else}
+              <Guess answerLength={game.answerLength} />
+            {/if}
+          {/each}
+        </div>
+      {/each}
+    </div>
+  {/each}
 </div>
+
+<Keyboard keyboard={game.keyboard} on:submit={submitGuess} on:input={onInput} />
--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,11 @@ export const ROUTES = {
   home: '/',
   fontViewer: '/font',
 } as const
+
+/**
+ * Maximum number of wordles that could be displayed for each row on the screen.
+ *
+ * Array index corresponds to the answer length of the wordle,
+ * i.e. `N_WORDLES_PER_ROW[2]` is #wordles/row when the answer length is 2.
+ */
+export const N_WORDLES_PER_ROW = [0, 4, 4, 3, 2, 2] as const
