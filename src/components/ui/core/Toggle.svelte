956a68ae4a820fa5391c3059a7787e3fa5b29c92
wolimst <64784258+wolimst@users.noreply.github.com>
2022-09-25 20:10:51 +0900

    Fix toggle component to use unique label id and dispatch toggle event

--- src/components/ui/core/Toggle.svelte
+++ src/components/ui/core/Toggle.svelte
@@ -1,11 +1,33 @@
+<script context="module" lang="ts">
+  let moduleCount = 0
+
+  type ToggleEvent = {
+    toggle: boolean
+  }
+</script>
+
 <script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+
   export let checked = false
+
+  const label = getLabel()
+
+  function getLabel(): string {
+    const label = `toggle-${moduleCount}`
+    moduleCount += 1
+    return label
+  }
+
+  const dispatch = createEventDispatcher<ToggleEvent>()
+
+  $: dispatch('toggle', checked)
 </script>
 
 <div class="tw-inline-flex tw-items-center tw-content-center tw-gap-3">
-  <label for="toggle" class="tw-cursor-pointer">
+  <label for={label} class="tw-cursor-pointer">
     <div class="tw-relative">
-      <input id="toggle" type="checkbox" class="tw-sr-only" bind:checked />
+      <input id={label} type="checkbox" class="tw-sr-only" bind:checked />
       <div class="tw-w-8 tw-h-3 tw-rounded-full tw-bg-app-text-secondary" />
       <div
         class="toggle-dot tw-absolute tw-w-5 tw-h-5 tw-rounded-full tw--left-1 tw--top-1 tw-transition tw-bg-app-neutral"
