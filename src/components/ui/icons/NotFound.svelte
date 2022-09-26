ffb49b569bd01af1e7057d5e39f3b3f758ac0bc6
wolimst <64784258+wolimst@users.noreply.github.com>
2022-09-27 04:11:33 +0900

    Add syllable based icons

---
+++ src/components/ui/icons/NotFound.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  export let widthPerSyllable: number = 48
+  export let gapRem: number = 0.125
+</script>
+
+<div class="tw-inline-flex tw-justify-center" style:gap={`${gapRem}rem`}>
+  <svg
+    class="icon tw-border tw-border-app-text-secondary tw-rounded-md tw-shadow-md"
+    xmlns="http://www.w3.org/2000/svg"
+    width={widthPerSyllable}
+    height={widthPerSyllable}
+    viewBox="-7.7 1.4 86 86"
+  >
+    <path
+      d="M19.73 34.78C19.73 26.78 25.92 22.39 35.28 22.39C44.64 22.39 50.83 26.78 50.83 34.78C50.83 42.77 42.84 47.16 35.28 47.16C27.72 47.16 19.73 42.77 19.73 34.78ZM13.46 34.78C13.46 47.30 24.26 52.42 35.28 52.42C46.30 52.42 57.10 47.30 57.10 34.78C57.10 22.25 46.30 17.14 35.28 17.14C24.26 17.14 13.46 22.25 13.46 34.78Z"
+      fill="var(--wordle-absent-color)"
+    />
+    <path
+      d="M6 72L65 72C66 72 67 71 67 69C67 67 66 66 65 66L38 66L38 58C38 56 37 56 35 56C33 56 32 56 32 58L32 66L6 66C4 66 3 67 3 69C3 71 4 72 6 72Z"
+      fill="var(--wordle-present-color)"
+    />
+  </svg>
+
+  <svg
+    class="icon tw-border tw-border-app-text-secondary tw-rounded-md tw-shadow-md"
+    xmlns="http://www.w3.org/2000/svg"
+    width={widthPerSyllable}
+    height={widthPerSyllable}
+    viewBox="-7.7 6.9 86 86"
+  >
+    <path
+      d="M12.38 31.90C10.94 31.90 10.15 32.90 10.15 34.49C10.15 36.07 10.94 37.08 12.38 37.08L13.25 37.08C12.60 38.66 12.24 40.61 12.24 42.55C12.24 49.18 16.49 51.70 35.28 51.70C43.49 51.70 51.48 50.83 57.60 49.10C59.11 48.67 59.47 47.38 59.18 45.94C58.90 44.57 57.89 43.63 56.02 44.14C50.40 45.65 42.98 46.44 35.28 46.44C21.10 46.44 18.65 45.00 18.65 41.83C18.65 40.18 18.86 38.52 19.22 37.08L51.48 37.08C55.58 37.08 57.60 35.06 57.60 30.96L57.60 24.12C57.60 20.02 55.58 18.00 51.48 18.00L13.90 18.00C12.46 18.00 11.66 19.01 11.66 20.59C11.66 22.18 12.46 23.18 13.90 23.18L49.10 23.18C50.62 23.18 51.26 23.83 51.26 25.34L51.26 29.74C51.26 31.25 50.62 31.90 49.10 31.90Z"
+      fill="var(--wordle-correct-color)"
+    />
+    <path
+      d="M47 82C49 82 50 81 50 79L50 62L65 62C66 62 67 61 67 60C67 58 66 57 65 57L6 57C4 57 3 58 3 60C3 61 4 62 6 62L20 62L20 79C20 81 22 82 24 82C25 82 27 81 27 79L27 62L44 62L44 79C44 81 45 82 47 82Z"
+      fill="var(--wordle-absent-color)"
+    />
+  </svg>
+</div>
---
+++ src/components/ui/icons/Wordle3x2.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  export let widthPerSyllable: number = 48
+  export let gapRem: number = 0.125
+</script>
+
+<div class="tw-inline-flex tw-justify-center" style:gap={`${gapRem}rem`}>
+  <svg
+    class="icon tw-border tw-border-app-text-secondary tw-rounded-md tw-shadow-md"
+    xmlns="http://www.w3.org/2000/svg"
+    width={widthPerSyllable}
+    height={widthPerSyllable}
+    viewBox="-8.8 3.2 86 86"
+  >
+    <path
+      d="M25.27 21.60C25.27 20.09 23.90 19.15 22.03 19.15C20.16 19.15 18.79 20.16 18.79 21.60L18.79 33.84C18.79 42.77 14.04 51.84 5.04 60.34C3.74 61.56 3.74 63.14 4.90 64.30C6.05 65.45 7.78 65.52 9.00 64.37C14.83 58.90 19.22 53.21 21.96 47.38L33.98 63.43C34.99 64.80 36.86 64.80 38.16 63.72C39.38 62.71 39.67 61.13 38.59 59.76L24.19 41.47C24.91 38.81 25.27 36.14 25.27 33.48Z"
+      fill="var(--wordle-correct-color)"
+    />
+    <path
+      d="M45 42L45 72C45 74 46 75 48 75C50 75 51 74 51 72L51 20C51 18 50 17 48 17C46 17 45 18 45 20L45 37L34 37C32 37 32 38 32 40C32 41 32 42 34 42ZM58 73C58 75 59 76 61 76C63 76 64 75 64 73L64 19C64 18 63 17 61 17C59 17 58 18 58 19Z"
+      fill="var(--wordle-absent-color)"
+    />
+  </svg>
+
+  <svg
+    class="icon tw-border tw-border-app-text-secondary tw-rounded-md tw-shadow-md"
+    xmlns="http://www.w3.org/2000/svg"
+    width={widthPerSyllable}
+    height={widthPerSyllable}
+    viewBox="-7.7 8.7 86 86"
+  >
+    <path
+      d="M54.94 22.61C56.38 22.61 57.17 21.60 57.17 20.02C57.17 18.43 56.38 17.42 54.94 17.42L12.82 17.42C11.38 17.42 10.58 18.43 10.58 20.02C10.58 21.60 11.38 22.61 12.82 22.61L13.54 22.61C12.60 24.48 12.02 26.93 12.02 29.38C12.02 36.94 15.91 39.74 35.28 39.74C43.20 39.74 51.05 38.81 56.30 37.37C57.96 36.86 58.32 35.50 58.03 34.20C57.74 32.90 56.81 32.04 55.01 32.47C49.68 33.77 42.70 34.56 35.28 34.56C21.10 34.56 18.36 33.05 18.36 29.38C18.36 27.07 18.79 24.62 19.44 22.61Z"
+      fill="var(--wordle-absent-color)"
+    />
+    <path
+      d="M6 50L65 50C66 50 67 49 67 47C67 46 66 45 65 45L6 45C4 45 3 46 3 47C3 49 4 50 6 50Z"
+      fill="var(--wordle-absent-color)"
+    />
+    <path
+      d="M13 58C13 60 13 61 15 61L50 61C51 61 52 62 52 63L52 66C52 67 51 68 50 68L14 68C12 68 11 69 11 70C11 72 12 73 14 73L14 73C14 74 14 76 14 78C14 83 17 86 36 86C44 86 52 85 58 84C59 83 60 82 59 81C59 79 58 78 57 79C51 80 43 81 36 81C22 81 20 80 20 76C20 75 20 74 20 73L52 73C56 73 58 71 58 67L58 61C58 58 56 56 52 56L15 56C13 56 13 57 13 58Z"
+      fill="var(--wordle-present-color)"
+    />
+  </svg>
+</div>
