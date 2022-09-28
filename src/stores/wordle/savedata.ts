b7aa018fe261b7b74422b4e7cce021779688c52c
wolimst <64784258+wolimst@users.noreply.github.com>
2022-09-29 05:14:59 +0900

    Implement encoded persistent store for daily wordle save data
    - Implement save/load for game data
    - Encode/decode on save/load to discourage data manipulation
    - Before loading, remove old save data to prevent the data size
      growing too large

---
+++ src/stores/wordle/savedata.ts
@@ -0,0 +1,66 @@
+import type * as Wordle from '@/lib/wordle'
+import { base64 } from '@/stores/encoder'
+import { persistentStore } from '@/stores/localStore'
+import { get } from 'svelte/store'
+
+export interface SaveStorage {
+  [gameId: string]: SaveData
+}
+
+export interface SaveData extends Wordle.GameData {
+  lastUpdatedDateISOString: string
+}
+
+const defaultStorage: SaveStorage = {}
+
+const store = persistentStore('savedata', defaultStorage, base64)
+
+export const savedata = {
+  subscribe: store.subscribe,
+  save: save,
+  load: load,
+}
+
+const DAY_MS = 24 * 60 * 60 * 1000
+const RETENTION_PERIOD_DAY = 14
+
+function save(data: Wordle.GameData) {
+  store.update((storage: SaveStorage): SaveStorage => {
+    storage[data.id] = {
+      ...data,
+      lastUpdatedDateISOString: new Date().toISOString(),
+    }
+    return storage
+  })
+}
+
+function load(gameId: string): Wordle.GameData | undefined {
+  store.update(removeOldData)
+  const data = structuredClone(get(savedata)[gameId])
+  if (!data) {
+    return undefined
+  } else {
+    return {
+      id: data.id,
+      nWordles: data.nWordles,
+      nGuesses: data.nGuesses,
+      answerLength: data.answerLength,
+      guesses: data.guesses,
+      status: data.status,
+      wordleData: data.wordleData,
+    }
+  }
+}
+
+function removeOldData(storage: SaveStorage): SaveStorage {
+  const now = new Date()
+  const entries = Object.entries(storage).filter(([, data]) => {
+    const lastUpdated = new Date(data.lastUpdatedDateISOString)
+    const dayDiff = Math.round(
+      Math.abs(now.getTime() - lastUpdated.getTime()) / DAY_MS
+    )
+    console.log(dayDiff)
+    return Number.isSafeInteger(dayDiff) && dayDiff < RETENTION_PERIOD_DAY
+  })
+  return Object.fromEntries(entries)
+}
