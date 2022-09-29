7714c186a5c0600bceedec5bb33f678f6b704a12
wolimst <64784258+wolimst@users.noreply.github.com>
2022-09-30 02:48:49 +0900

    Add license and copyright notice

--- src/components/nav/Config.svelte
+++ src/components/nav/Config.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Copyright from './Copyright.svelte'
   import Badge from '@/components/ui/core/Badge.svelte'
   import ClickButton from '@/components/ui/core/ClickButton.svelte'
   import Modal from '@/components/ui/core/Modal.svelte'
@@ -18,7 +19,7 @@
 </ClickButton>
 
 <Modal bind:open title="설정" widthRem={25} heightRem={30}>
-  <div class="tw-w-full tw-inline-flex tw-flex-col tw-gap-4">
+  <div class="tw-w-full tw-h-full tw-inline-flex tw-flex-col tw-gap-4">
     <Toggle checked={$config.darkTheme} on:toggle={config.toggleTheme}>
       다크 모드
     </Toggle>
@@ -26,5 +27,9 @@
     <Toggle bind:checked={$config.switchEnterAndBackspacePosition}>
       <Badge>입력</Badge> 과 <Badge>삭제</Badge> 키 위치 바꾸기
     </Toggle>
+
+    <div class="tw-mt-auto">
+      <Copyright />
+    </div>
   </div>
 </Modal>
---
+++ src/components/nav/Copyright.svelte
@@ -0,0 +1,20 @@
+<div class="tw-text-xs tw-text-app-text-secondary">
+  <p>© 2022-present Woohyun Lim & 한들 Contributors</p>
+  <p class="tw-mt-1">
+    단어 리스트는
+    <a
+      href="https://www.korean.go.kr/front/etcData/etcDataView.do?mn_id=208&etc_seq=71"
+      class="tw-underline tw-underline-offset-1 tw-text-app-text-secondary"
+    >
+      한국어 학습용 어휘 목록
+    </a>
+    과
+    <a
+      href="https://kbig.kr/portal/kbig/datacube/niadict.page"
+      class="tw-underline tw-underline-offset-1 tw-text-app-text-secondary"
+    >
+      한글 형태소 사전
+    </a>
+    을 편집하여 제작하였습니다.
+  </p>
+</div>
