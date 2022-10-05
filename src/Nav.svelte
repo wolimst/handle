228d25a99e76ff017e1c2fca5612ebc4a538d2ba
wolimst <64784258+wolimst@users.noreply.github.com>
2022-10-05 23:01:27 +0900

    Change page routing to use hash based router

--- src/Nav.svelte
+++ src/Nav.svelte
@@ -13,14 +13,14 @@
   class="tw-inline-flex tw-justify-between tw-gap-4 tw-w-full tw-px-6 tw-py-2 tw-border-b tw-border-solid tw-border-app-text-secondary"
 >
   <div class="tw-inline-flex tw-gap-4">
-    <LinkButton link={ROUTES.home} underline={false}>
+    <LinkButton url={ROUTES.home} useRouter underline={false}>
       <AppIcon width={32} />
       <span class="tw-ml-2 tw-font-medium">{APP_NAME}</span>
     </LinkButton>
   </div>
   <div class="tw-inline-flex tw-gap-4">
     {#if !PRODUCTION}
-      <LinkButton link={ROUTES.fontViewer}>
+      <LinkButton url={ROUTES.fontViewer} useRouter>
         <FontIcon />
       </LinkButton>
     {/if}
@@ -29,7 +29,7 @@
     <Help />
     <Config />
 
-    <LinkButton link="https://github.com/wolimst/handle">
+    <LinkButton url="https://github.com/wolimst/handle">
       <GitHubIcon />
     </LinkButton>
   </div>
