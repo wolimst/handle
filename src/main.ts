3380883aff9f4281ebc083033378c3eb5a9a62cb
wolimst <64784258+wolimst@users.noreply.github.com>
2022-11-02 22:25:10 +0900

    Implement PWA update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import App from './App.svelte'
 import './app.css'
 import { applyConfig } from './stores/app'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  async onNeedRefresh() {
+    const answer = confirm('새로운 버전이 있어요. 업데이트를 적용할까요?')
+    if (answer) {
+      await updateSW(true)
+    }
+  },
+})
 
 function initApp() {
   applyConfig()
