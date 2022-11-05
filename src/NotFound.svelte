f612217bede3b284c9002d65792bc802732286c5
wolimst <64784258+wolimst@users.noreply.github.com>
2022-11-05 23:57:36 +0900

    Center page contents

--- src/NotFound.svelte
+++ src/NotFound.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-  class="tw-container tw-p-10 tw-flex tw-flex-col tw-justify-center tw-items-center tw-gap-6"
+  class="tw-container tw-mx-auto tw-p-10 tw-flex tw-flex-col tw-justify-center tw-items-center tw-gap-6"
 >
   <NotFoundIcon widthPerSyllable={128} gapRem={0.75} />
 
