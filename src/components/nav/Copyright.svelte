391f9ba1a7688734b0b4a082136761299ed97006
wolimst <64784258+wolimst@users.noreply.github.com>
2024-04-02 20:38:36 +0900

    feat(word): update word list for length 2

--- src/components/nav/Copyright.svelte
+++ src/components/nav/Copyright.svelte
@@ -7,16 +7,36 @@
       target="_blank"
       class="tw-underline tw-underline-offset-1 tw-text-app-text-secondary"
     >
-      한국어 학습용 어휘 목록
-    </a>
-    과
+      한국어 학습용 어휘 목록</a
+    >,
     <a
       href="https://kbig.kr/portal/kbig/datacube/niadict.page"
       target="_blank"
       class="tw-underline tw-underline-offset-1 tw-text-app-text-secondary"
     >
-      한글 형태소 사전
-    </a>
-    을 편집하여 제작하였습니다.
+      한글 형태소 사전</a
+    >,
+    <a
+      href="https://krdict.korean.go.kr/kor/mainAction"
+      target="_blank"
+      class="tw-underline tw-underline-offset-1 tw-text-app-text-secondary"
+    >
+      한국어기초사전</a
+    >,
+
+    <a
+      href="https://stdict.korean.go.kr/main/main.do"
+      target="_blank"
+      class="tw-underline tw-underline-offset-1 tw-text-app-text-secondary"
+    >
+      표준국어대사전</a
+    >,
+    <a
+      href="https://opendict.korean.go.kr/main"
+      target="_blank"
+      class="tw-underline tw-underline-offset-1 tw-text-app-text-secondary"
+    >
+      우리말샘</a
+    >의 단어를 편집하여 제작하였습니다.
   </p>
 </div>
