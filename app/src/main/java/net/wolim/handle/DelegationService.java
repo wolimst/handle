5e07a6c5684f2ec9e37adb9fa4c22ae75f8f3982
wolimst <64784258+wolimst@users.noreply.github.com>
2024-05-04 23:44:54 +0900

    app(android): init

---
+++ app/src/main/java/net/wolim/handle/DelegationService.java
@@ -0,0 +1,14 @@
+package net.wolim.handle;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
