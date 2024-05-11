6aac30bf1104bc5c569c946a8c05d0e7b66d4d7e
wolimst <64784258+wolimst@users.noreply.github.com>
2024-05-11 12:12:48 +0900

    chore: add analytics

--- src/App.svelte
+++ src/App.svelte
@@ -24,6 +24,14 @@
   routes.set('*', PageNotFound)
 </script>
 
+{#if PRODUCTION}
+  <script
+    defer
+    src="https://static.cloudflareinsights.com/beacon.min.js"
+    data-cf-beacon={'{"token": "56ce7e917d004e3a99327438feb854f8"}'}
+  ></script>
+{/if}
+
 <Nav />
 
 <main
